e6b31b79b7ef98d6c1417c7bc120541a653ace1b
Josh Menden <josh.menden@hey.com>
2023-12-06 10:16:55 -0700

    brute force pt2

--- day-6/a.rb
+++ day-6/a.rb
@@ -5,7 +5,8 @@ require "active_support/all"
 require_relative "../aoc_solution"
 
 class Day < AOCSolution
-  def calculate_wins(t, d)
+  def calculate_wins(race)
+    t, d = race
     t.times.reduce(0) { |n, ms| ((t - ms) * ms > d) ? n + 1 : n + 0 }
   end
 
@@ -15,13 +16,17 @@ class Day < AOCSolution
       .map(&:split)
       .map {|arr| arr.map(&:to_i)}
       .transpose
-      .reduce(1) { |n, race| n * calculate_wins(race[0], race[1]) }
+      .reduce(1) { |n, race| n * calculate_wins(race) }
   end
 
   def pt2
+    calculate_wins(@data.scan(/\w+:\s+(\d+(?:\s+\d+)*)/)
+      .flatten
+      .map {|str| str.gsub(" ", "")}
+      .map(&:to_i))
   end
 end
 
 filename = "input.txt"
-# filename = "sample.txt"
+filename = "sample.txt"
 Day.new(filename).solve!
