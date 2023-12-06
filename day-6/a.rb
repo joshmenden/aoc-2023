abaa66dfcad8e4879bbabb8d2e1ec5d5f5ad2e4f
Josh Menden <josh.menden@hey.com>
2023-12-06 10:52:27 -0700

    Use quadratic formula for pt2

--- day-6/a.rb
+++ day-6/a.rb
@@ -20,13 +20,29 @@ class Day < AOCSolution
   end
 
   def pt2
-    calculate_wins(@data.scan(/\w+:\s+(\d+(?:\s+\d+)*)/)
-      .flatten
-      .map {|str| str.gsub(" ", "")}
-      .map(&:to_i))
+    # brute force ~3.3s to solve
+    # calculate_wins(@data.scan(/\w+:\s+(\d+(?:\s+\d+)*)/)
+    #         .flatten
+    #         .map {|str| str.gsub(" ", "")}
+    #         .map(&:to_i))
+
+    # quadratic formula: ax^2 + bx + c = 0
+    t, d = @data.scan(/\w+:\s+(\d+(?:\s+\d+)*)/)
+                .flatten
+                .map {|str| str.gsub(" ", "")}
+                .map(&:to_i)
+
+    # record = held * (time - held)
+    # record = (held*time) - (held^2)
+    # 0 = -held^2 + (time*held) - record
+    # for me, held is x
+    # x = [-b ± sqrt(b^2 - 4ac)] / (2a)
+    held1 = (-t + Math.sqrt(t ** 2 - 4 * d)) / -2
+    held2 = (-t - Math.sqrt(t ** 2 - 4 * d)) / -2
+    held2.floor - held1.ceil + 1
   end
 end
 
 filename = "input.txt"
-filename = "sample.txt"
+# filename = "sample.txt"
 Day.new(filename).solve!
