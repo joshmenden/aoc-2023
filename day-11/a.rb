8a61e1abc016df1bf1855f71477265765d5f426c
Josh Menden <josh.menden@hey.com>
2023-12-11 14:14:45 -0700

    Use manhattan geometry instead of bfs, pt2 complete

--- day-11/a.rb
+++ day-11/a.rb
@@ -4,128 +4,40 @@ require "byebug"
 require "active_support/all"
 require_relative "../aoc_solution"
 
+# the trick for this one is manhattan geometry
+# https://en.wikipedia.org/wiki/Taxicab_geometry
+
 class Day < AOCSolution
-  def expand
-    y_candidates = []
-    twod_arr(@data).transpose.each_with_index { |r, ind| y_candidates << ind if r.all? {|val| val == "."} }
+  def parse_data
+    @cosmos = twod_arr(@data)
+    @column_expansion_indices = @cosmos.transpose.each_with_index.select { |r, _| r.all? { |val| val == "." } }.map(&:last).sort.reverse
+    @row_expansion_indices = @cosmos.each_with_index.select { |r, _| r.all? { |v| v == "." } }.map(&:last)
+  end
 
-    x_expanded = twod_arr(@data)
-      .each_with_object([]) { |r, newarr| newarr << r.dup << (r.all? {|c| c == "." } ? r.dup : nil) }
+  def shortest_path(expansion_factor, (a, b))
+    row_dist = (b[0] - a[0]).abs + expand(expansion_factor, @row_expansion_indices, b[0], a[0])
+    column_dist = (b[1] - a[1]).abs + expand(expansion_factor, @column_expansion_indices, b[1], a[1])
+
+    row_dist + column_dist
+  end
+
+  def expand(expansion_factor, indices, c1, c2)
+    sorted = [c1, c2].sort
+    (expansion_factor - 1) * (indices.count { |num| (sorted[0]..sorted[1]).include?(num) })
+  end
+
+  def pt1(expansion_factor: 2)
+    @cosmos
+      .each_with_index
+      .flat_map { |r, rind| r.each_with_index.map { |val, cind| [rind, cind] if val == "#" } }
       .compact
-
-    cosmos = []
-    x_expanded.each do |r|
-      # require "byebug"; byebug;
-      y_candidates.sort.reverse_each do |c|
-        r.insert(c, r[c])
-      end
-      cosmos << r
-    end
-
-    cosmos
-  end
-
-  def find_galaxies(cosmos)
-    galaxies = []
-    cosmos.each_with_index { |r, rind| r.each_with_index { |val, cind| galaxies << [rind, cind] if val == "#" } }
-
-    galaxies
-  end
-
-  def relevant_children(pos, goal)
-    children = []
-    r, c = pos
-    gr, gc = goal
-
-    directions = []
-    if gr >= r
-      directions << :down
-    elsif gr < r
-      directions << :up
-    end
-
-    if gc >= c
-      directions << :right
-    elsif gc < c
-      directions << :left
-    end
-
-    case directions
-    when [:down, :right]
-      return [
-        [r + 1, c],
-        [r, c + 1],
-      ]
-    when [:down, :left]
-      return [
-        [r + 1, c],
-        [r, c - 1],
-      ]
-    when [:up, :right]
-      return [
-        [r - 1, c],
-        [r, c + 1],
-      ]
-    when [:up, :left]
-      return [
-        [r - 1, c],
-        [r, c - 1],
-      ]
-    end
-  end
-
-  def shortest_path(cosmos, combo)
-    start, goal = combo
-    queue = [[start]]
-    visited = Set.new
-    visited.add(start)
-
-    while !queue.empty?
-      path = queue.shift
-      pos = path.last
-
-      if pos == goal
-        @shortest_paths[[start, goal]] = path
-        @shortest_paths[[goal, start]] = path
-        return path
-      end
-
-      if @shortest_paths.key?([pos, goal])
-        # require "byebug"; byebug;
-        shortest = path.dup.concat(@shortest_paths[[pos, goal]].dup).uniq
-        @shortest_paths[[start, goal]] = shortest
-        @shortest_paths[[goal, start]] = shortest
-
-        return shortest
-      end
-
-      relevant_children(pos, goal).each do |new_pos|
-        # require "byebug"; byebug;
-        next if visited.include?(new_pos)
-        next if !within_bounds?(cosmos, new_pos[0], new_pos[1])
-
-        visited << new_pos
-        new_path = path.dup
-        new_path << new_pos
-        queue << new_path
-      end
-    end
-  end
-
-  def pt1
-    @shortest_paths = {}
-    cosmos = expand 
-    galaxies = find_galaxies(cosmos)
-    combinations = galaxies.combination(2).to_a
-    counter = 0
-    combinations.reduce(0) do |acc, combo|
-      counter += 1
-      puts "Processing combination number #{counter}, (#{combo}) of #{combinations.count}"
-      acc + shortest_path(cosmos, combo).length - 1
-    end
+      .combination(2)
+      .to_a
+      .reduce(0) { |acc, combo| acc + shortest_path(expansion_factor, combo) }
   end
 
   def pt2
+    pt1(expansion_factor: 1_000_000)
   end
 end
 
