da27668633d4fee419784082301b351da2fc923f
Josh Menden <josh.menden@hey.com>
2023-12-13 13:03:09 -0700

    day12 boilerpalte

---
+++ day-12/a.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require "byebug"
+require "active_support/all"
+require_relative "../aoc_solution"
+
+class Day < AOCSolution
+  def parse_data
+    # access @data to parse input
+  end
+
+  def pt1
+  end
+
+  def pt2
+  end
+end
+
+filename = "sample.txt"
+# filename = "input.txt"
+Day.new(filename).solve!
