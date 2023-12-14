3993ae139f3084746c22f7f7b578b582c12916b6
Josh Menden <josh.menden@hey.com>
2023-12-13 20:37:33 -0700

    pt1 faster, still not fast enough for pt2

--- day-12/a.rb
+++ day-12/a.rb
@@ -6,16 +6,27 @@ require_relative "../aoc_solution"
 
 class Day < AOCSolution
   def place_group(row, groups)
+    cache_key = [row.dup, groups.dup]
+    return @place_group_cache[cache_key] if @place_group_cache.key?(cache_key)
+
     spring_size = groups.shift
     possibles = place_spring(row, spring_size)
 
-    return possibles if groups.empty?
-    return possibles.map { |p| place_group(p, groups.dup) }
+    result = if groups.empty?
+               possibles
+             else
+               possibles.map { |p| place_group(p, groups.dup) }
+             end
+
+    @place_group_cache[cache_key] = result
   end
 
   def place_spring(row, gsize)
+    cache_key = [row.dup, gsize.dup]
+    return @place_spring_cache[cache_key] if @place_spring_cache.key?(cache_key)
+
     possibles = []
-    row.each_cons(gsize).with_index do |group, ind|
+    row.chars.each_cons(gsize).with_index do |group, ind|
       next unless group.all? {|el| el == "?" || el == "#" }
       next unless ind == 0 || [".", "?"].include?(row[ind - 1]) # preceded by operational spring
       next unless ind == (row.size - gsize) || [".", "?"].include?(row[ind + gsize]) # followed by operational spring
@@ -25,30 +36,36 @@ class Day < AOCSolution
       possibles << poss
     end
 
-    possibles
+    @place_spring_cache[cache_key] = possibles
   end
 
-  def pt1
+  def solve(factor)
+    @place_group_cache = {}
+    @place_spring_cache = {}
     @data
       .scan(/([.#?]*) ((?:\d,?)*)/)
       .map do |m|
         [
-          m[0].split(""),
-          m[1].split(",").map(&:to_i)
+          ([m[0]] * factor).join("?"),
+          m[1].split(",").map(&:to_i) * factor
         ]
       end
+      # .yield_self { |rows| [rows[0]] }
       .reduce(0) do |acc, (row, groups)|
-        acc + place_group(row, groups.dup)
-                .flatten(groups.length - 1)
-                .uniq
-                .map(&:join)
-                .select {|r| r.scan(/#+/).map {|m| m.length } == groups }
-                .count
+        acc + place_group(row, groups.dup.sort.reverse)
+          .flatten(groups.length - 1)
+          .uniq
+          .select {|r| r.scan(/#+/).map {|m| m.length } == groups }
+          .count
       end
   end
 
+  def pt1
+    solve(1)
+  end
+
   def pt2
-    # what if removed all groups that already had a match
+    # solve(5)
   end
 end
 
